ed603b4a42ed4ebeda613be78b1d9cb50fefd0a7
Serhii <serhii.lohachov@gmail.com>
2022-10-01 04:49:07 +0300

    Added to Github

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Homework for Lesson 4 </title>
 </head>
 
 <body>
+    <h5>Заняття 4. ООП</h5>
+    <hr />
     <?php
     class Employee
     {
