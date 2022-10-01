560ca7c0c59a0a9336102e6d664f8ae3aed42f86
Serhii <serhii.lohachov@gmail.com>
2022-10-01 04:39:32 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    class Employee
+    {
+        public $name;
+        public $age;
+        public $salary;
+    }
+
+    $employee1 = new Employee;
+    $employee1->name = "John";
+    $employee1->age = 25;
+    $employee1->salary = 1000;
+
+    $employee2 = new Employee;
+    $employee2->name = "Eric";
+    $employee2->age = 26;
+    $employee2->salary = 2000;
+
+    echo "Сума зарплат {$employee1->name} та {$employee2->name} дорівнює: " . $employee1->salary . " + " . $employee2->salary . " = " .  ($employee1->salary + $employee2->salary) . " <br />";
+
+    echo "Загальний вік {$employee1->name} та {$employee2->name} дорівнює: " . $employee1->age . " + " . $employee2->age . " = " . ($employee1->age + $employee2->age) . " <br />";
+    ?>
+
+</body>
+
+</html>
